de7874afcb8ae7990a3ccccd9629ec99e1eaf7d4
hello.patz@gmail.com <hello.patz@gmail.com@bd63f230-7cd2-159e-b929-9929ae436ba3>
2011-06-20 23:52:20 +0000

    Refractorred the implementation of ISynchronousInvoke of control.
    git-svn-id: http://mai.googlecode.com/svn/trunk@5 bd63f230-7cd2-159e-b929-9929ae436ba3

--- mUI/mUI/Forms/Control.cpp
+++ mUI/mUI/Forms/Control.cpp
@@ -503,58 +503,6 @@ void Control::OnKeyPress( KeyPressEventArgs* e )
 	KeyPress(this, e);
 }
 
-// ----------------------------------------------------- //
-class Control::Task
-{
-public:
-	Task(const Delegate<>& method)
-	{
-		method_ = method;
-	}
-	void Invoke()
-	{
-		method_();
-	}
-
-private:
-	Delegate<> method_;
-};
-
-void Control::InvokeHelper( const Delegate<>& method, bool fSynchronous )
-{
-	{
-		AutoLock lock(this);
-		qutaskinvoke_.push(new Task(method));
-	}
-
-	if (!InvokeRequired())
-	{
-		_InvokeAll();
-	}
-}
-
-void Control::_InvokeAll()
-{
-	AutoLock lock(this);
-	while (!qutaskinvoke_.empty())
-	{
-		Task* task = qutaskinvoke_.front();
-		qutaskinvoke_.pop();
-		assert(task != NULL);
-		task->Invoke();
-		delete task;
-	}
-}
-
-bool Control::InvokeRequired() const
-{
-	return Thread::get_ManagedThreadID() != thread_;
-}
-
-void Control::Invoke( const Delegate<void>& method )
-{
-	InvokeHelper(method, false);
-}
 // ------------------------------------------------- //
 
 Control::ControlCollection::~ControlCollection()
