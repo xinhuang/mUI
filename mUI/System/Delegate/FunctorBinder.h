b18ffc2b458d1d28c4fa695ce78da2fcffe4d9d1
hello.patz@gmail.com <hello.patz@gmail.com@bd63f230-7cd2-159e-b929-9929ae436ba3>
2011-10-01 07:36:05 +0000

    Binding for 3 arguments.
    git-svn-id: http://mai.googlecode.com/svn/trunk@29 bd63f230-7cd2-159e-b929-9929ae436ba3

--- mUI/System/Delegate/FunctorBinder.h
+++ mUI/System/Delegate/FunctorBinder.h
@@ -19,8 +19,47 @@
 
 #define BINDER_ROOT
 
-#define TPL_T_RET_ARG       typename TRet = void, typename TArg0 = void, typename TArg1 = void
-#define TPL_T_ARG           typename TArg0 = void, typename TArg1 = void
+#define TPL_T_RET_ARG       typename TRet = void, typename TArg0 = void, typename TArg1 = void, typename TArg2 = void
+#define TPL_T_ARG           typename TArg0 = void, typename TArg1 = void, typename TArg2 = void
+#define T_RET_ARG           TRet, TArg0, TArg1, TArg2
+#define T_ARG               TArg0, TArg1, TArg2
+
+#define ARG_DECL            const TArg0& arg0, const TArg1& arg1, const TArg1& arg2
+#define ARG                 arg0, arg1, arg2
+#define FUN_T_ARG_ARG       TArg0 arg0, TArg1 arg1, TArg2 arg2
+
+#define MEMBER_ARG_DECL     TArg0 arg0_; TArg1 arg1_; TArg2 arg2_
+#define MEMBER_ARG          arg0_, arg1_, arg2_
+#define MEMBER_ARG_ASSIGN   do { arg0_ = arg0; arg1_ = arg1; arg2_ = arg2; } while (0)
+#define ARGUMENTS_EQUAL     args.arg0_ == arg0_ && args.arg1_ == arg1_ && args.arg2_ == arg2_
+
+#define BINDING_ARG         args_.arg0_, args_.arg1_, args_.arg2_
+
+#                           include "FunctorBinderImpl.h"
+
+#undef TPL_T_RET_ARG
+#undef TPL_T_ARG
+#undef T_RET_ARG
+#undef T_ARG 
+
+#undef ARG_DECL 
+#undef ARG 
+#undef FUN_T_ARG_ARG
+
+#undef MEMBER_ARG_DECL
+#undef MEMBER_ARG
+#undef MEMBER_ARG_ASSIGN
+#undef ARGUMENTS_EQUAL
+
+#undef BINDING_ARG
+
+
+#undef BINDER_ROOT
+
+// ----------------------------------------------------
+
+#define TPL_T_RET_ARG       typename TRet, typename TArg0, typename TArg1
+#define TPL_T_ARG           typename TArg0, typename TArg1
 #define T_RET_ARG           TRet, TArg0, TArg1
 #define T_ARG               TArg0, TArg1
 
