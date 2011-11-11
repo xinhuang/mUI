a9e297d179a0e3e1f892609eb675348983f42a39
hello.patz@gmail.com <hello.patz@gmail.com@bd63f230-7cd2-159e-b929-9929ae436ba3>
2011-11-11 17:45:53 +0000

    UT for Random: Next_WithUpperBound Next_WithUpperAndLowerBound
    git-svn-id: http://mai.googlecode.com/svn/trunk@34 bd63f230-7cd2-159e-b929-9929ae436ba3

--- UnitTest/Random.cpp
+++ UnitTest/Random.cpp
@@ -27,6 +27,7 @@ TEST_F(RandomTest, Constructor_WhenTypical)
 TEST_F(RandomTest, Constructor_SameSeedSameSequence)
 {
 	int arbitrary_seed = 10;
+	delete random_;
 	random_ = new Random(arbitrary_seed);
 	Random comparison(arbitrary_seed);
 
@@ -47,4 +48,21 @@ TEST_F(RandomTest, Next_WhenTypical)
 
 TEST_F(RandomTest, Next_WithUpperBound)
 {
+	int arbitrary_upper_bound = 20;
+	for (int i = 0; i < 10; ++i)
+	{
+		ASSERT_GT(arbitrary_upper_bound, random_->Next(arbitrary_upper_bound));
+	}
+}
+
+TEST_F(RandomTest, Next_WithUpperAndLowerBound)
+{
+	int arbitrary_upper_bound = 20;
+	int arbitrary_lower_bound = 19;
+	for (int i = 0; i < 10; ++i)
+	{
+		int random_value = random_->Next(arbitrary_lower_bound, arbitrary_upper_bound);
+		ASSERT_GT(arbitrary_upper_bound, random_value);
+		ASSERT_LE(arbitrary_lower_bound, random_value);
+	}
 }
