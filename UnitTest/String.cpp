d04e20ec9c28fd364a879696ee58d060dbaf7096
hello.patz@gmail.com <hello.patz@gmail.com@bd63f230-7cd2-159e-b929-9929ae436ba3>
2011-11-11 17:58:24 +0000

    UT for String: Constructor_WhenConstructUsingWcharArray
    git-svn-id: http://mai.googlecode.com/svn/trunk@36 bd63f230-7cd2-159e-b929-9929ae436ba3

--- UnitTest/String.cpp
+++ UnitTest/String.cpp
@@ -25,3 +25,16 @@ TEST_F(StringTest, Constructor_WhenTypical)
 	ASSERT_FALSE(NULL == string_);
 }
 
+TEST_F(StringTest, Constructor_WhenConstructUsingWcharArray)
+{
+	const wchar_t* init_wstring = L"ArbitraryString";
+	wchar_t check_for_const[128];
+	wcscpy(check_for_const, init_wstring);
+	
+	string_ = new String(init_wstring);
+
+	ASSERT_EQ(init_wstring, *string_);
+	ASSERT_STREQ(check_for_const, init_wstring);
+	ASSERT_EQ(check_for_const, *string_);
+}
+
