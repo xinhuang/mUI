5c42330664c825c390a785ebc4a4b193b90cfb6c
xinhuang <walkaniceday@gmail.com>
2011-11-22 18:59:49 +0800

    Use FindGTest.cmake in MineGame_UnitTest.

---
+++ CMakeModules/FindGMock.cmake
@@ -0,0 +1,19 @@
+# Locate GMock Library
+
+SET(GMOCK_FOUND FALSE)
+
+if (WIN32)
+	find_path(GMOCK_INCLUDE_DIR gmock.h
+			"${CMAKE_SOURCE_DIR}/dependency/include/gmock"
+			)
+
+	find_path(GMOCK_LIBRARY_DIR 
+			NAMES gmock.lib gmock_main.lib
+			HINTS
+			"${CMAKE_SOURCE_DIR}/dependency/lib/gmock"
+			)
+	
+	IF (GMOCK_INCLUDE_DIR AND GMOCK_LIBRARY_DIR)
+		SET(GMOCK_FOUND TRUE)
+	ENDIF()
+endif ()
