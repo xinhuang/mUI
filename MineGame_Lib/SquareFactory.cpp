3bf492bae72d65a110464e5976a36369be684024
xinhuang <walkaniceday@gmail.com>
2011-11-23 01:10:11 +0800

    CreateSquares_MineField1x1AllMine

--- MineGame_Lib/SquareFactory.cpp
+++ MineGame_Lib/SquareFactory.cpp
@@ -1,6 +1,12 @@
 #include "SquareFactory.h"
 
+#include "NumberSquare.h"
+#include "BlankSquare.h"
+#include "MineSquare.h"
+
 vector<ISquare*> SquareFactory::CreateSquares(MGame* game, MineField* mineField)
 {
-	return vector<ISquare*>();
+	vector<ISquare*> squares;
+	squares.push_back(new MineSquare(game, mineField, 0, 0));
+	return squares;
 }
--- MineGame_UnitTest/SquareFactoryTest.cpp
+++ MineGame_UnitTest/SquareFactoryTest.cpp
@@ -4,17 +4,56 @@
 #include <gmock/gmock.h>
 using ::testing::_;
 
+#include <MGame.h>
+#include <MineField.h>
 #include <SquareFactory.h>
 #include <ISquare.h>
+#include <MineSquare.h>
+#include <BlankSquare.h>
+#include <NumberSquare.h>
 
 #include "mocks/MineFieldMock.h"
 
 class SquareFactoryTest : public testing::Test
 {
 public:
+	virtual void SetUp()
+	{
+		_factory = new SquareFactory();
+	}
+
+	virtual void TearDown()
+	{
+		delete _factory;
+		_factory = NULL;
+		for(size_t i = 0; i < _squares.size(); ++i)
+			delete _squares[i];
+		_squares.clear();
+	}
+
+protected:
+	SquareFactory* _factory;
+	vector<ISquare*> _squares;
 };
 
 TEST_F(SquareFactoryTest, Constructor_Typical)
 {
-	SquareFactory factory;
+	ASSERT_TRUE(NULL != _factory);
+}
+
+TEST_F(SquareFactoryTest, CreateSquares_MineField1x1AllMine)
+{
+	MGame game;
+	game.set_MineFieldWidth(1);
+	game.set_MineFieldHeight(1);
+	game.set_MineTotal(1);
+	MineField mineField(&game);
+	
+	_squares = _factory->CreateSquares(&game, &mineField);
+
+	ASSERT_EQ(1, _squares.size());
+	ASSERT_EQ(0, _squares[0]->get_Row());
+	ASSERT_EQ(0, _squares[0]->get_Column());
+	MineSquare* mineSquare = dynamic_cast<MineSquare*>(_squares[0]);
+	ASSERT_TRUE(NULL != mineSquare);
 }
