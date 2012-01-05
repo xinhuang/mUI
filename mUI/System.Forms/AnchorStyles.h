3784dcd059cceff597e5550a83f9f946c9ad5fb7
xinhuang <walkaniceday@gmail.com>
2012-01-05 23:28:16 +0800

    AnchorStyles

---
+++ mUI/System.Forms/AnchorStyles.h
@@ -0,0 +1,20 @@
+#ifndef __MUI_SYSTEM_FORMS_ANCHORSTYLES_H__
+#define __MUI_SYSTEM_FORMS_ANCHORSTYLES_H__
+
+namespace mUI{ namespace System{  namespace Forms{
+
+namespace AnchorStyles
+{
+	enum Enum
+	{
+		Top,
+		Botton,
+		Left,
+		Right,
+		None
+	};
+}
+
+}}}
+
+#endif
