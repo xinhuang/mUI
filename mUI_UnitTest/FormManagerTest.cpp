824823253cee13fd9bc353ff0e4dbcb2aa4569ae
xinhuang <walkaniceday@gmail.com>
2012-01-08 13:47:06 +0800

    TEST_F(FormManagerTest, MapWindowPoint_WhenTranslateFromControlToScreen)

--- mUI_UnitTest/FormManagerTest.cpp
+++ mUI_UnitTest/FormManagerTest.cpp
@@ -29,15 +29,35 @@ TEST_F(FormManagerTest, Constructor_Typical)
 	ASSERT_TRUE(null != _sut);
 }
 
-TEST_F(FormManagerTest, MapWindowPoint_Typical)
+TEST_F(FormManagerTest, MapWindowPoint_WhenTranslateFromFormToScreen)
 {
 	Point formLocation(100, 100);
+	Point offsetPoint(7, 7);
 	Form* container = new Form();
 	container->set_Size(Size(100, 100));
 	container->set_Location(formLocation);
 
 	Point screenPoint = 
-		FormManager::get_Instance().MapWindowPoint(container->get_Handle(), null, Point::Empty);
+		FormManager::get_Instance().MapWindowPoint(container->get_Handle(), null, offsetPoint);
 
-	ASSERT_EQ(formLocation, screenPoint);
+	ASSERT_EQ(formLocation + offsetPoint, screenPoint);
+}
+
+TEST_F(FormManagerTest, MapWindowPoint_WhenTranslateFromControlToScreen)
+{
+	Point offsetPoint(7, 7);
+	Point formLocation(100, 100);
+	Form* container = new Form();
+	container->set_Size(Size(100, 100));
+	container->set_Location(formLocation);
+	Point elementLocation(13, 13);
+	Control* element = new Control();
+	element->set_Size(Size(10, 10));
+	element->set_Location(elementLocation);
+	container->Controls.Add(*element);
+
+	Point screenPoint = 
+		FormManager::get_Instance().MapWindowPoint(element->get_Handle(), null, offsetPoint);
+
+	ASSERT_EQ(formLocation + elementLocation + offsetPoint, screenPoint);
 }
